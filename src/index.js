6c59a13e1b5d588bd7836453a11008010109decc
Dawid <dawidgawronskicontact@gmail.com>
2023-04-15 23:01:29 +0100

    Change UI class to module

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import "./style.css";
 
-class TodoList {
+class TodoListClass {
   constructor() {
     this.projects = [];
+  }
+
+  addProject(name) {
+    this.projects.push(new ProjectClass(name));
+  }
+
+  deleteProject(index) {
+    this.projects.splice(index, 1);
   }
 
   getProjects() {
@@ -10,48 +18,26 @@ class TodoList {
   }
 
   printProjects() {
-    console.table(this.getProjects());
-  }
-
-  addProject(name) {
-    this.projects.push(new Project(name));
-  }
-
-  getProject(name) {
-    return this.projects.find((project) => project.name === name);
-  }
-
-  printProject(name) {
-    console.log(this.getProject(name));
-  }
-
-  getProjectIndex(name) {
-    return this.projects.indexOf(
-      this.projects.find((project) => project.name === name)
-    );
-  }
-
-  printProjectIndex(name) {
-    console.log(this.getProjectIndex(name));
+    console.table(this.projects);
   }
 }
 
-class Project {
+class ProjectClass {
   constructor(name) {
     this.name = name;
     this.tasks = [];
   }
 
+  addTask(name) {
+    this.tasks.push(new TaskClass(name));
+  }
+
+  deleteTask(index) {
+    this.tasks.splice(index, 1);
+  }
+
   getName() {
     return this.name;
-  }
-
-  printName() {
-    console.log(this.getName());
-  }
-
-  addTask(name) {
-    this.tasks.push(new Task(name));
   }
 
   getTasks() {
@@ -59,196 +45,260 @@ class Project {
   }
 
   printTasks() {
-    console.table(this.getTasks());
-  }
-
-  getTask(name) {
-    return this.tasks.find((task) => task.name === name);
-  }
-
-  printTask(name) {
-    console.log(this.getTask(name));
+    console.log(this.getTasks());
   }
 }
 
-class Task {
+class TaskClass {
   constructor(name) {
     this.name = name;
+    this.done = false;
   }
 
   getName() {
     return this.name;
   }
-
-  printName() {
-    console.log(this.getName());
-  }
 }
 
-class UI {
-  constructor() {
-    this.currentProject = 0;
-  }
+const TodoList = new TodoListClass();
+TodoList.addProject("Home");
+TodoList.addProject("Work");
+TodoList.addProject("Gym");
 
-  createAside() {
-    const aside = document.createElement("aside");
-    aside.innerHTML = `
-    <form>
-        <input 
-            type="text" 
-            id="projectNameInput"
-        >
-        <button 
-            type="button"
-            id="projectAddButton"
-        >Add Project</button>
-    </form>
+TodoList.projects[0].addTask("Washing");
+TodoList.projects[0].addTask("Cleaning");
+TodoList.projects[0].addTask("Cooking");
 
-    <ul id="projectsList">
-    
-    </ul>
-    `;
+TodoList.projects[1].addTask("DELL");
+TodoList.projects[1].addTask("ThinkPad");
+TodoList.projects[1].addTask("Lenovo");
 
-    return aside;
-  }
+TodoList.projects[2].addTask("Barbell press");
+TodoList.projects[2].addTask("Dumbell press");
+TodoList.projects[2].addTask("Squat");
 
-  appendAside() {
-    document.body.appendChild(this.createAside());
-  }
+TodoList.printProjects();
 
-  createMain() {
-    const main = document.createElement("main");
-    main.innerHTML = `
+const UI = (() => {
+  let currentProject = 0;
 
-    <ul id="tasksList">
-    
-    </ul>
-    `;
+  const _init = () => {
+    _cacheDOM();
+    _render();
+    _bindEvents();
+  };
 
-    return main;
-  }
-
-  appendMain() {
-    document.body.appendChild(this.createMain());
-  }
-
-  createProject(name) {
-    return `
-    <li class="projects-list-item">
-    ${name}
-    </li>`;
-  }
-
-  renderProjects(TodoList) {
-    this.cacheDOM().projectsList.innerHTML = "";
-    TodoList.projects.forEach((project) => {
-      this.cacheDOM().projectsList.innerHTML += this.createProject(
-        project.getName()
-      );
-    });
-
-    this.cacheDOM();
-  }
-
-  changeCurrentProject(TodoList, index) {
-    this.currentProject = index;
-    this.renderTasks(TodoList);
-  }
-
-  createTask(name) {
-    return `
-    <li class="tasks-list-item">
-    ${name}
-    </li>`;
-  }
-
-  renderTasks(TodoList) {
-    this.cacheDOM().tasksList.innerHTML = "";
-    TodoList.projects[this.currentProject].tasks.forEach((task) => {
-      this.cacheDOM().tasksList.innerHTML += this.createTask(task.getName());
-    });
-
-    this.cacheDOM();
-  }
-
-  cacheDOM() {
+  const _cacheDOM = () => {
     const projectNameInput = document.querySelector("#projectNameInput");
+
     const projectAddButton = document.querySelector("#projectAddButton");
+
     const projectsList = document.querySelector("#projectsList");
-    const projectsListItems = Array.from(
+
+    const projectListItems = Array.from(
       document.querySelectorAll(".projects-list-item")
     );
+
     const tasksList = document.querySelector("#tasksList");
+
     const tasksListItems = Array.from(
       document.querySelectorAll(".tasks-list-item")
     );
 
+    const projectsListItemDelete = Array.from(
+      document.querySelectorAll(".projects-list-item-delete")
+    );
+
+    const tasksListItemDelete = Array.from(
+      document.querySelectorAll(".tasks-list-item-delete")
+    );
+
+    const taskNameInput = document.querySelector("#taskNameInput");
+
+    const taskAddButton = document.querySelector("#taskAddButton");
+
+    const tasksListItemDone = Array.from(
+      document.querySelectorAll(".tasks-list-item-done")
+    );
+
     return {
       projectNameInput,
-      projectAddButton,
-      projectsList,
-      projectsListItems,
-      tasksList,
-      tasksListItems,
-    };
-  }
 
-  eventListeners(TodoList) {
-    // ADD PROJECT VIA FORM
-    this.cacheDOM().projectAddButton.addEventListener("click", () => {
-      TodoList.addProject(this.cacheDOM().projectNameInput.value);
-      this.renderProjects(TodoList);
-      this.eventListeners(TodoList);
+      projectAddButton,
+
+      projectsList,
+
+      projectListItems,
+
+      tasksList,
+
+      tasksListItems,
+
+      projectsListItemDelete,
+
+      tasksListItemDelete,
+
+      taskNameInput,
+
+      taskAddButton,
+
+      tasksListItemDone,
+    };
+  };
+
+  const _render = () => {
+    document.body.innerHTML = "";
+
+    // render aside
+    document.body.appendChild(_createAside());
+
+    // render projects
+    TodoList.getProjects().forEach((project) => {
+      _cacheDOM().projectsList.appendChild(_createProject(project));
     });
-    // CHANGE TASKS WHEN YOU CLICK DIFFERENT PROJECT
-    this.cacheDOM().projectsListItems.forEach((project) => {
-      project.addEventListener("click", (e) => {
-        const index = this.cacheDOM().projectsListItems.indexOf(e.target);
-        this.changeCurrentProject(TodoList, index);
+
+    // render main
+    document.body.appendChild(_createMain());
+
+    // render tasks
+    TodoList.projects[currentProject]
+      .getTasks()
+      .forEach((task) => _cacheDOM().tasksList.appendChild(_createTask(task)));
+
+    _cacheDOM();
+  };
+
+  const _bindEvents = () => {
+    // add project
+    _cacheDOM().projectAddButton.addEventListener("click", () => {
+      if (_cacheDOM().projectNameInput.value === "") {
+        alert("You must name your project!");
+      } else {
+        TodoList.addProject(_cacheDOM().projectNameInput.value);
+      }
+      _init();
+    });
+
+    // delete project
+    _cacheDOM().projectsListItemDelete.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        // this logic doesnt make sense but works
+        if (TodoList.projects.length > 1) {
+          const index = _cacheDOM().projectsListItemDelete.indexOf(e.target);
+          TodoList.deleteProject(index);
+          _init();
+        } else {
+          alert("You can delete project only if there is more than one");
+        }
       });
     });
 
-    this.cacheDOM();
-  }
-}
+    // add task
+    _cacheDOM().taskAddButton.addEventListener("click", () => {
+      TodoList.projects[currentProject].addTask(
+        _cacheDOM().taskNameInput.value
+      );
+      _init();
+    });
 
-const newUI = new UI();
+    // delete task
+    _cacheDOM().tasksListItemDelete.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        const index = _cacheDOM().tasksListItemDelete.indexOf(e.target);
+        TodoList.projects[currentProject].deleteTask(index);
+        _init();
+      });
+    });
 
-(function init() {
-  const newTodoList = new TodoList();
-  newTodoList.addProject("Gym");
-  newTodoList.addProject("Home");
-  newTodoList.addProject("Work");
+    // change projects
+    _cacheDOM().projectListItems.forEach((project) => {
+      project.addEventListener("click", (e) => {
+        const index = _cacheDOM().projectListItems.indexOf(e.target);
+        currentProject = index;
+        _init();
+      });
+    });
 
-  // GYM TASKS
-  newTodoList.projects[newTodoList.getProjectIndex("Gym")].addTask(
-    "Bench Press"
-  );
-  newTodoList.projects[newTodoList.getProjectIndex("Gym")].addTask(
-    "Barbell Squat"
-  );
-  newTodoList.projects[newTodoList.getProjectIndex("Gym")].addTask("Deadlift");
+    // change done
+    _cacheDOM().tasksListItemDone.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        const index = _cacheDOM().tasksListItemDone.indexOf(e.target);
 
-  // HOME TASKS
-  newTodoList.projects[newTodoList.getProjectIndex("Home")].addTask("Cleaning");
-  newTodoList.projects[newTodoList.getProjectIndex("Home")].addTask(
-    "Washing Dishes"
-  );
-  newTodoList.projects[newTodoList.getProjectIndex("Home")].addTask("Hovering");
+        //  i need to fix this retared shit
+        TodoList.projects[currentProject].tasks[index].done === false
+          ? (TodoList.projects[currentProject].tasks[index].done = true)
+          : (TodoList.projects[currentProject].tasks[index].done = false);
 
-  // WORK TASKS
-  newTodoList.projects[newTodoList.getProjectIndex("Work")].addTask("DELLs");
-  newTodoList.projects[newTodoList.getProjectIndex("Work")].addTask(
-    "ThinkPads"
-  );
-  newTodoList.projects[newTodoList.getProjectIndex("Work")].addTask("Lenovos");
+        console.log(TodoList.projects[currentProject].tasks[index].done);
 
-  newUI.appendAside();
-  newUI.appendMain();
-  newUI.cacheDOM();
+        _init();
+      });
+    });
+  };
 
-  newUI.renderProjects(newTodoList);
-  newUI.renderTasks(newTodoList);
+  const _createAside = () => {
+    const aside = document.createElement("aside");
+    aside.innerHTML = `
+      <form>
+        <input type="text" id="projectNameInput">
+        <button type="button" id="projectAddButton">ADD PROJECT</button>
+      </form>
+      <ul id="projectsList"></ul>
+    `;
+    return aside;
+  };
 
-  newUI.eventListeners(newTodoList);
+  const _createProject = (project) => {
+    const component = document.createElement("li");
+    component.innerHTML = `
+      <span class="projects-list-item">${project.getName()}</span>
+      <button type="button" class="projects-list-item-delete">X</button>
+    `;
+
+    return component;
+  };
+
+  const _createMain = () => {
+    const main = document.createElement("main");
+    main.innerHTML = `
+    <form>
+        <input type="text" id="taskNameInput">
+        <button type="button" id="taskAddButton">ADD TASK</button>
+      </form>
+      <ul id="projectsList"></ul>
+      <ul id="tasksList"></ul>
+    `;
+    return main;
+  };
+
+  const _createTask = (task) => {
+    const component = document.createElement("li");
+
+    const doneBtn = document.createElement("button");
+    doneBtn.type = "button";
+    doneBtn.classList = "tasks-list-item-done";
+    doneBtn.innerHTML = "DONE?";
+    component.appendChild(doneBtn);
+
+    const name = document.createElement("span");
+    name.classList = "tasks-list-item";
+    name.innerHTML = task.getName();
+    if (task.done === true) {
+      name.style.textDecoration = "line-through";
+    } else {
+      name.style.textDecoration = "none";
+    }
+
+    component.appendChild(name);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.type = "button";
+    deleteBtn.classList = "tasks-list-item-delete";
+    deleteBtn.innerHTML = "X";
+    component.appendChild(deleteBtn);
+
+    return component;
+  };
+
+  _init();
 })();
