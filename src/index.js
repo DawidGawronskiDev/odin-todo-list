3d9cec8da241a730d814be4b6bfd5186eb99cfae
Dawid <dawidgawronskicontact@gmail.com>
2023-04-16 21:26:12 +0100

    .

--- src/index.js
+++ src/index.js
@@ -1,64 +1,12 @@
 import "./style.css";
 
-class TodoListClass {
-  constructor() {
-    this.projects = [];
-  }
+import TodoListClass from "./Components/Todo";
+import ProjectClass from "./Components/Project";
 
-  addProject(name) {
-    this.projects.push(new ProjectClass(name));
-  }
-
-  deleteProject(index) {
-    this.projects.splice(index, 1);
-  }
-
-  getProjects() {
-    return this.projects;
-  }
-
-  printProjects() {
-    console.table(this.projects);
-  }
-}
-
-class ProjectClass {
-  constructor(name) {
-    this.name = name;
-    this.tasks = [];
-  }
-
-  addTask(name) {
-    this.tasks.push(new TaskClass(name));
-  }
-
-  deleteTask(index) {
-    this.tasks.splice(index, 1);
-  }
-
-  getName() {
-    return this.name;
-  }
-
-  getTasks() {
-    return this.tasks;
-  }
-
-  printTasks() {
-    console.log(this.getTasks());
-  }
-}
-
-class TaskClass {
-  constructor(name) {
-    this.name = name;
-    this.done = false;
-  }
-
-  getName() {
-    return this.name;
-  }
-}
+import deleteIcon from "./Assets/icons/delete-icon.svg";
+import editIcon from "./Assets/icons/edit-icon.svg";
+import doneIcon from "./Assets/icons/done-icon.svg";
+import addIcon from "./Assets/icons/add-icon.svg";
 
 const TodoList = new TodoListClass();
 TodoList.addProject("Home");
@@ -77,228 +25,286 @@ TodoList.projects[2].addTask("Barbell press");
 TodoList.projects[2].addTask("Dumbell press");
 TodoList.projects[2].addTask("Squat");
 
-TodoList.printProjects();
-
 const UI = (() => {
-  let currentProject = 0;
+  let current = 0;
+  let addFormDisplay = false;
 
   const _init = () => {
-    _cacheDOM();
     _render();
+    _cacheDOM();
     _bindEvents();
-  };
-
-  const _cacheDOM = () => {
-    const projectNameInput = document.querySelector("#projectNameInput");
-
-    const projectAddButton = document.querySelector("#projectAddButton");
-
-    const projectsList = document.querySelector("#projectsList");
-
-    const projectListItems = Array.from(
-      document.querySelectorAll(".projects-list-item")
-    );
-
-    const tasksList = document.querySelector("#tasksList");
-
-    const tasksListItems = Array.from(
-      document.querySelectorAll(".tasks-list-item")
-    );
-
-    const projectsListItemDelete = Array.from(
-      document.querySelectorAll(".projects-list-item-delete")
-    );
-
-    const tasksListItemDelete = Array.from(
-      document.querySelectorAll(".tasks-list-item-delete")
-    );
-
-    const taskNameInput = document.querySelector("#taskNameInput");
-
-    const taskAddButton = document.querySelector("#taskAddButton");
-
-    const tasksListItemDone = Array.from(
-      document.querySelectorAll(".tasks-list-item-done")
-    );
-
-    return {
-      projectNameInput,
-
-      projectAddButton,
-
-      projectsList,
-
-      projectListItems,
-
-      tasksList,
-
-      tasksListItems,
-
-      projectsListItemDelete,
-
-      tasksListItemDelete,
-
-      taskNameInput,
-
-      taskAddButton,
-
-      tasksListItemDone,
-    };
   };
 
   const _render = () => {
     document.body.innerHTML = "";
-
-    // render aside
     document.body.appendChild(_createAside());
+    document.body.appendChild(_createMain());
+    document.body.appendChild(_createAddProjectContainer());
 
     // render projects
-    TodoList.getProjects().forEach((project) => {
-      _cacheDOM().projectsList.appendChild(_createProject(project));
+    TodoList.projects.forEach((project) => {
+      _cacheDOM().projectList.appendChild(_createProject(project));
     });
 
-    // render main
-    document.body.appendChild(_createMain());
-
     // render tasks
-    TodoList.projects[currentProject]
-      .getTasks()
-      .forEach((task) => _cacheDOM().tasksList.appendChild(_createTask(task)));
+    TodoList.projects[current].tasks.forEach((task) => {
+      _cacheDOM().taskList.appendChild(_createTask(task));
+    });
+  };
 
-    _cacheDOM();
+  const _cacheDOM = () => {
+    const projectNameInput = document.querySelector("#projectNameInput");
+    const projectAddBtn = document.querySelector("#projectAddBtn");
+    const projectList = document.querySelector("#projectList");
+
+    const projectListItem = Array.from(
+      document.querySelectorAll(".project-list-item")
+    );
+    const projectListItemDel = Array.from(
+      document.querySelectorAll(".project-list-item-del")
+    );
+    const projectListItemEdit = Array.from(
+      document.querySelectorAll(".project-list-item-edit")
+    );
+    const projectListItemNameInput = Array.from(
+      document.querySelectorAll(".project-list-item-name-input")
+    );
+    const projectListItemNameBtn = Array.from(
+      document.querySelectorAll(".project-list-item-name-btn")
+    );
+
+    const taskNameInput = document.querySelector("#taskNameInput");
+    const taskAddBtn = document.querySelector("#taskAddBtn");
+    const taskList = document.querySelector("#taskList");
+
+    const taskListItem = Array.from(
+      document.querySelectorAll(".task-list-item")
+    );
+    const taskListItemDel = Array.from(
+      document.querySelectorAll(".task-list-item-del")
+    );
+    const taskListItemDone = Array.from(
+      document.querySelectorAll(".task-list-item-done")
+    );
+
+    const addProjectContainer = document.querySelector("#addPprojectContainer");
+    const projectAddFormShow = Array.from(
+      document.querySelectorAll(".project-add-form-show")
+    );
+
+    return {
+      projectNameInput,
+      projectAddBtn,
+      projectList,
+
+      projectListItem,
+      projectListItemDel,
+      projectListItemEdit,
+      projectListItemNameInput,
+      projectListItemNameBtn,
+
+      taskNameInput,
+      taskAddBtn,
+      taskList,
+
+      taskListItem,
+      taskListItemDel,
+      taskListItemDone,
+
+      addProjectContainer,
+      projectAddFormShow,
+    };
   };
 
   const _bindEvents = () => {
+    // change current project
+    _cacheDOM().projectListItem.forEach((item) => {
+      item.addEventListener("click", (e) => {
+        const index = _cacheDOM().projectListItem.indexOf(e.target);
+        current = index;
+        _init();
+      });
+    });
+
     // add project
-    _cacheDOM().projectAddButton.addEventListener("click", () => {
-      if (_cacheDOM().projectNameInput.value === "") {
-        alert("You must name your project!");
-      } else {
-        TodoList.addProject(_cacheDOM().projectNameInput.value);
-      }
+    _cacheDOM().projectAddBtn.addEventListener("click", () => {
+      TodoList.projects.push(
+        new ProjectClass(_cacheDOM().projectNameInput.value)
+      );
+      addFormDisplay === false
+        ? (addFormDisplay = true)
+        : (addFormDisplay = false);
       _init();
     });
 
     // delete project
-    _cacheDOM().projectsListItemDelete.forEach((button) => {
+    _cacheDOM().projectListItemDel.forEach((button) => {
       button.addEventListener("click", (e) => {
-        // this logic doesnt make sense but works
         if (TodoList.projects.length > 1) {
-          const index = _cacheDOM().projectsListItemDelete.indexOf(e.target);
+          const index = _cacheDOM().projectListItemDel.indexOf(e.target);
+          index === 0 ? (current = 0) : (current = index - 1);
+          _render();
           TodoList.deleteProject(index);
           _init();
-        } else {
-          alert("You can delete project only if there is more than one");
         }
       });
     });
 
-    // add task
-    _cacheDOM().taskAddButton.addEventListener("click", () => {
-      TodoList.projects[currentProject].addTask(
-        _cacheDOM().taskNameInput.value
-      );
-      _init();
+    // project turn on edit mode
+    // _cacheDOM().projectListItemEdit.forEach((button) => {
+    //   button.addEventListener("click", (e) => {
+    //     const index = _cacheDOM().projectListItemEdit.indexOf(e.target);
+    //     TodoList.projects[index].changeEdit();
+    //     console.log(TodoList.projects[index]);
+    //     _init();
+    //   });
+    // });
+
+    // project edit name
+    _cacheDOM().projectListItemNameBtn.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        const index = _cacheDOM().projectListItemNameBtn.indexOf(e.target);
+        TodoList.projects[index].name =
+          _cacheDOM().projectListItemNameInput[index].value;
+        TodoList.projects[index].changeEdit();
+        _init();
+      });
     });
+
+    // add task
+    // _cacheDOM().taskAddBtn.addEventListener("click", () => {
+    //   TodoList.projects[current].addTask(_cacheDOM().taskNameInput.value);
+    //   _init();
+    // });
 
     // delete task
-    _cacheDOM().tasksListItemDelete.forEach((button) => {
+    _cacheDOM().taskListItemDel.forEach((button) => {
       button.addEventListener("click", (e) => {
-        const index = _cacheDOM().tasksListItemDelete.indexOf(e.target);
-        TodoList.projects[currentProject].deleteTask(index);
+        const index = _cacheDOM().taskListItemDel.indexOf(e.target);
+        TodoList.projects[current].deleteTask(index);
         _init();
       });
     });
 
-    // change projects
-    _cacheDOM().projectListItems.forEach((project) => {
-      project.addEventListener("click", (e) => {
-        const index = _cacheDOM().projectListItems.indexOf(e.target);
-        currentProject = index;
-        _init();
-      });
-    });
-
-    // change done
-    _cacheDOM().tasksListItemDone.forEach((button) => {
+    // change done task
+    _cacheDOM().taskListItemDone.forEach((button) => {
       button.addEventListener("click", (e) => {
-        const index = _cacheDOM().tasksListItemDone.indexOf(e.target);
-
-        //  i need to fix this retared shit
-        TodoList.projects[currentProject].tasks[index].done === false
-          ? (TodoList.projects[currentProject].tasks[index].done = true)
-          : (TodoList.projects[currentProject].tasks[index].done = false);
-
-        console.log(TodoList.projects[currentProject].tasks[index].done);
-
+        const index = _cacheDOM().taskListItemDone.indexOf(e.target);
+        TodoList.projects[current].tasks[index].changeDone();
         _init();
       });
     });
-  };
 
-  const _createAside = () => {
-    const aside = document.createElement("aside");
-    aside.innerHTML = `
-      <form>
-        <input type="text" id="projectNameInput">
-        <button type="button" id="projectAddButton">ADD PROJECT</button>
-      </form>
-      <ul id="projectsList"></ul>
-    `;
-    return aside;
+    // show add project container
+    _cacheDOM().projectAddFormShow.forEach((button) => {
+      button.addEventListener("click", () => {
+        addFormDisplay === false
+          ? (addFormDisplay = true)
+          : (addFormDisplay = false);
+        _init();
+      });
+    });
   };
 
   const _createProject = (project) => {
     const component = document.createElement("li");
-    component.innerHTML = `
-      <span class="projects-list-item">${project.getName()}</span>
-      <button type="button" class="projects-list-item-delete">X</button>
-    `;
+
+    if (project.edit === false) {
+      component.innerHTML += `<span class="project-list-item">${project.getName()}</span>`;
+      component.innerHTML += `<img src="${editIcon}" class="project-list-item-edit">`;
+      component.innerHTML += `<img src="${deleteIcon}" class="project-list-item-del">`;
+    } else {
+      component.innerHTML = `
+      <button type="button" class="project-list-item-edit">EDIT</button>
+      <input type="text" class="project-list-item-name-input" value="${project.getName()}">
+      <button type="button" class="project-list-item-name-btn">CHANGE</button>
+      `;
+    }
 
     return component;
   };
 
-  const _createMain = () => {
-    const main = document.createElement("main");
-    main.innerHTML = `
-    <form>
-        <input type="text" id="taskNameInput">
-        <button type="button" id="taskAddButton">ADD TASK</button>
-      </form>
-      <ul id="projectsList"></ul>
-      <ul id="tasksList"></ul>
-    `;
-    return main;
-  };
-
   const _createTask = (task) => {
     const component = document.createElement("li");
-
-    const doneBtn = document.createElement("button");
-    doneBtn.type = "button";
-    doneBtn.classList = "tasks-list-item-done";
-    doneBtn.innerHTML = "DONE?";
-    component.appendChild(doneBtn);
-
-    const name = document.createElement("span");
-    name.classList = "tasks-list-item";
-    name.innerHTML = task.getName();
-    if (task.done === true) {
-      name.style.textDecoration = "line-through";
+    if (task.done === false) {
+      component.innerHTML += `<span class="task-list-item">${task.getName()}</span>`;
     } else {
-      name.style.textDecoration = "none";
+      component.innerHTML += `<span class="task-list-item" style="text-decoration: line-through;">${task.getName()}</span>`;
     }
 
-    component.appendChild(name);
+    component.innerHTML += `<img src="${doneIcon}" class="task-list-item-done">`;
+    component.innerHTML += `<img src="${deleteIcon}" class="task-list-item-del">`;
 
-    const deleteBtn = document.createElement("button");
-    deleteBtn.type = "button";
-    deleteBtn.classList = "tasks-list-item-delete";
-    deleteBtn.innerHTML = "X";
-    component.appendChild(deleteBtn);
+    return component;
+  };
+
+  const _createAside = () => {
+    const component = document.createElement("aside");
+    component.classList = "aside";
+    component.innerHTML = `
+    <div class="container">
+      <span class="heading-l">Projects</span>
+      <img src="${addIcon}" class="project-add-form-show">
+      </div>
+      <ul id="projectList"></ul>
+    </div>
+    `;
+    return component;
+  };
+
+  const _createMain = () => {
+    const component = document.createElement("main");
+    component.classList = "main";
+    component.innerHTML = `
+    <div class="container">
+      <span class="main-header heading-l">Tasks</span>
+      <ul id="taskList"></ul>
+    </div>
+    `;
+    return component;
+  };
+
+  const _createAddProjectContainer = () => {
+    const component = document.createElement("div");
+    component.id = "addProjectContainer";
+    component.innerHTML = `
+    <img src="${deleteIcon}" class="project-add-form-show">
+    <form>
+      <input type="text" id="projectNameInput" placeholder="e.g. New Project" />
+      <button type="button" id="projectAddBtn">ADD PROJECT</button>
+    </form>
+    `;
+
+    if (!addFormDisplay) {
+      component.style.display = "none";
+    } else {
+      component.style.display = "block";
+    }
+
+    return component;
+  };
+
+  const _createAddTaskContainer = () => {
+    const component = document.createElement("div");
+    component.id = "addtaskContainer";
+    component.innerHTML = `
+    <img src="${deleteIcon}" class="task-add-form-show">
+    <form>
+      <input type="text" id="taskNameInput" placeholder="e.g. New Project" />
+      <button type="button" id="taskAddBtn">ADD PROJECT</button>
+    </form>
+    `;
+
+    if (!addFormDisplay) {
+      component.style.display = "none";
+    } else {
+      component.style.display = "block";
+    }
 
     return component;
   };
 
   _init();
 })();
+
+// DODAC FORMULARZ DO DODAWANIA TASKOW !!!
